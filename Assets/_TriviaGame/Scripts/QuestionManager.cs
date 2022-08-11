04986874b42d384995811820ca31d0ce68635866
recanar <360arslan@gmail.com>
2022-08-11 04:24:10 +0300

    Move Ui codes to question screen script

--- Assets/_TriviaGame/Scripts/QuestionManager.cs
+++ Assets/_TriviaGame/Scripts/QuestionManager.cs
@@ -2,50 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using System.Text.RegularExpressions;
-using System;
 using UnityEngine.UI;
-using System.Web;
 
 public class QuestionManager : MonoBehaviour
 {
-    private enum AnswerStates
-    {
-        NoAnswer,
-        WrongAnswer,
-        CorrectAnswer
-    }
     [System.Serializable]
     private class Answers
     {
         public List<Button> answerButtons;
         [HideInInspector] public List<TextMeshProUGUI> answerTexts;
-        public Color correctAnswerColor;
-        public Color wrongAnswerColor;
-        public Color buttonStartColor;
-        public Color answerTextStartColor;
     }
-
-    public static QuestionManager instance;
-
-    [SerializeField] private TextMeshProUGUI questionText;
-    [SerializeField] private TextMeshProUGUI categoryText;
-    [SerializeField] private TextMeshProUGUI questionNumberText;
-    [SerializeField] private GameObject quitPanel;
-
-    [SerializeField] private GameObject showAnswerPanel;
-
-    [SerializeField] private GameObject countdownPanel;
-    [SerializeField] private TextMeshProUGUI countdownText;
-
     [SerializeField] private Answers answers;
 
-    [SerializeField] private Button nextQuestionButton;
+    public static QuestionManager instance;//singleton
+    public QuestionScreenUI questionScreen;
 
+    [SerializeField] private GameObject quitPanel;
+    [SerializeField] private GameObject countdownPanel;
 
     private int currentQuestionIndex;
     private Question question;
-    private Coroutine noAnswerRoutine;//stop if got answered
+    private Coroutine noAnswerRoutine;//stop if question got answered
 
     private void Awake()
     {
@@ -53,14 +30,13 @@ public class QuestionManager : MonoBehaviour
         {
             instance = this;
         }
-
     }
     private void Start()
     {
         foreach (var button in answers.answerButtons)
         {
             var btnText = button.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
-            answers.answerTexts.Add(btnText);//get buttons' texts
+            answers.answerTexts.Add(btnText);//get buttons' texts and add to list
             button.onClick.AddListener(() => AnswerButtonClicked(button, btnText));//add click event to answer buttons
         }
     }
@@ -68,58 +44,52 @@ public class QuestionManager : MonoBehaviour
     public void GenerateQuestions(int id)
     {
         currentQuestionIndex = 0;//make current index 0 at start quiz
-        question = ApiHelper.GetQuestions(id);
-        ShowNextQuestion();
+        question = ApiHelper.GetQuestions(id);//get data from api
+
+        questionScreen.SendQuestionData(question,currentQuestionIndex);//send data to UI manager and question Ui
+        noAnswerRoutine = StartCoroutine(NoAnswerCheckRoutine());//start question countdown after question appeared
     }
-    private void ShowNextQuestion()
-    {
-        questionNumberText.text = (currentQuestionIndex + 1) + "/10";
-        questionText.text = HttpUtility.HtmlDecode(question.results[currentQuestionIndex].question);//using striphtml method for remove html entity codes
-        categoryText.text = question.results[currentQuestionIndex].category;
-        answers.answerTexts[0].text = HttpUtility.HtmlDecode(question.results[currentQuestionIndex].incorrect_answers[0]);
-        answers.answerTexts[1].text = HttpUtility.HtmlDecode(question.results[currentQuestionIndex].incorrect_answers[1]);
-        answers.answerTexts[2].text = HttpUtility.HtmlDecode(question.results[currentQuestionIndex].incorrect_answers[2]);
-        answers.answerTexts[3].text = HttpUtility.HtmlDecode(question.results[currentQuestionIndex].correct_answer);
-        noAnswerRoutine= StartCoroutine(CheckNoAnswer());
-    }
-    private IEnumerator CheckNoAnswer()
+    private IEnumerator NoAnswerCheckRoutine()
     {
         yield return new WaitForSeconds(60);
-        StartCoroutine(NextQuestionRoutine(AnswerStates.NoAnswer));
+        StartCoroutine(NextQuestionRoutine());
     }
     public void AnswerButtonClicked(Button pressedButton, TextMeshProUGUI btnPressedText)
     {
         StopCoroutine(noAnswerRoutine);
-        showAnswerPanel.SetActive(true);
-        pressedButton.interactable = false;//deselect button
-        pressedButton.interactable = true;
+        foreach (var btn in answers.answerButtons)
+        {
+            btn.interactable = false;
+        }
         if (btnPressedText.text == question.results[currentQuestionIndex].correct_answer.ToString())//compare chosen answer and correct answer
         {
-            ChangeButtonColor(pressedButton, answers.correctAnswerColor);
-            btnPressedText.color = Color.white;
-            StartCoroutine(NextQuestionRoutine(AnswerStates.CorrectAnswer));
-            ChangeButtonColor(nextQuestionButton, answers.correctAnswerColor);
-            nextQuestionButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.white;
-            nextQuestionButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Correct";
+            StartCoroutine(NextQuestionRoutine());
             countdownPanel.SetActive(true);
+            questionScreen.CorrectAnswer();//correct answer UI 
         }
         else
         {
-            ChangeButtonColor(pressedButton, answers.wrongAnswerColor);
-            btnPressedText.color = Color.white;
-            StartCoroutine(NextQuestionRoutine(AnswerStates.WrongAnswer));
-            ChangeButtonColor(nextQuestionButton,answers.wrongAnswerColor);
-            nextQuestionButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().color = Color.white;
-            nextQuestionButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Wrong";
+            StartCoroutine(NextQuestionRoutine());
             countdownPanel.SetActive(true);
-            for (int i = 0; i < answers.answerTexts.Count; i++)
-            {
-                if (answers.answerTexts[i].text == question.results[currentQuestionIndex].correct_answer.ToString())
-                {
-                    ChangeButtonColor(answers.answerButtons[i], answers.correctAnswerColor);
-                    answers.answerTexts[i].color = Color.white;
-                }
-            }
+            questionScreen.WrongAnswer(pressedButton,btnPressedText);//wrong answer UI
+        }
+    }
+    private IEnumerator NextQuestionRoutine()
+    {
+        yield return new WaitForSeconds(1);
+        questionScreen.NextQuestionCountdownStart();//UI
+        yield return new WaitForSeconds(3);
+        countdownPanel.SetActive(false);
+        currentQuestionIndex++;
+        questionScreen.NextQuestionCountdownEnd();//UI
+        if (currentQuestionIndex == question.results.Count)
+        {
+            currentQuestionIndex = 0;
+            GameManager.instance.BackToMenu();//game end back to menu
+        }
+        else
+        {
+            questionScreen.PrintQuestionScreen(currentQuestionIndex);//if quiz haven't finished go next question 
         }
     }
     public void QuitButtonClicked()
@@ -129,55 +99,5 @@ public class QuestionManager : MonoBehaviour
     public void CloseQuitPanel()
     {
         quitPanel.SetActive(false);
-    }
-    private IEnumerator NextQuestionRoutine(AnswerStates answerState)
-    {
-        yield return new WaitForSeconds(1);
-
-        ChangeButtonColor(nextQuestionButton, answers.buttonStartColor);
-        nextQuestionButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Next question After:" + 3 + " Seconds";
-        //countdownText.text = "Next question After:\n" + 3 + " Seconds";
-        yield return new WaitForSeconds(1);
-        nextQuestionButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Next question After:" + 2 + " Seconds";
-        //countdownText.text = "Next question After:\n" + 2 + " Seconds";
-        yield return new WaitForSeconds(1);
-        nextQuestionButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = "Next question After:" + 1 + " Seconds";
-        //countdownText.text = "Next question After:\n" + 1 + " Seconds";
-        yield return new WaitForSeconds(1);
-        showAnswerPanel.SetActive(false);
-        countdownPanel.SetActive(false);
-        currentQuestionIndex++;
-        foreach (var button in answers.answerButtons)//reset colors before load new question
-        {
-            ChangeButtonColor(button, answers.buttonStartColor);
-        }
-        foreach (var answerText in answers.answerTexts)
-        {
-            answerText.color = answers.answerTextStartColor;
-        }
-        if (currentQuestionIndex == question.results.Count)
-        {
-            currentQuestionIndex = 0;
-            GameManager.instance.BackToMenu();//game end back to menu
-        }
-        else
-        {
-            ShowNextQuestion();
-        }
-    }
-    public string StripHTML(string input)
-    {
-        if (input == null)
-        {
-            return string.Empty;
-        }
-        return Regex.Replace(input, "&.*?;", String.Empty);
-
-    }
-    private void ChangeButtonColor(Button btn, Color color)
-    {
-        ColorBlock colorVar = btn.colors;
-        colorVar.normalColor = color;
-        btn.colors = colorVar;
     }
 }
--- Assets/_TriviaGame/Scripts/QuestionScreenUI.cs
+++ Assets/_TriviaGame/Scripts/QuestionScreenUI.cs
@@ -1,18 +1,111 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Web;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionScreenUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Question questionsData;
+
+    [SerializeField] private TextMeshProUGUI questionText;
+    [SerializeField] private TextMeshProUGUI questionNumberText;
+    [SerializeField] private TextMeshProUGUI categoryText;
+    [SerializeField] private GameObject clickBlockPanel;
+    [SerializeField] private Button nextQuestionButton;
+    private TextMeshProUGUI nextQuestionButtonText;
+    public List<Button> answerButtons;
+    public List<TextMeshProUGUI> answerTexts;
+
+    public Color correctAnswerColor;
+    public Color wrongAnswerColor;
+    public Color buttonStartColor;
+    public Color answerTextStartColor;
+
+    private void Start()
     {
-        
+        for (int i = 0; i < answerButtons.Count; i++)
+        {
+            answerTexts.Add(answerButtons[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>());
+        }
+        nextQuestionButtonText= nextQuestionButton.transform.GetChild(0).GetComponent<TextMeshProUGUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SendQuestionData(Question questions,int questionIndex)
     {
-        
+        questionsData=questions;
+        PrintQuestionScreen(questionIndex);
+    }
+    public void PrintQuestionScreen(int questionIndex)
+    {
+        PrintCategory(questionIndex);
+        PrintQuestionNumber(questionIndex);
+        PrintQuestion(questionIndex);
+        PrintAnswerTexts(questionIndex);
+    }
+    public void CorrectAnswer()
+    {
+        clickBlockPanel.gameObject.SetActive(true);
+        answerTexts[3].color = Color.white;
+        ChangeButtonColor(answerButtons[3], correctAnswerColor);
+        ChangeButtonColor(nextQuestionButton, correctAnswerColor);
+        nextQuestionButtonText.color = Color.white;
+        nextQuestionButtonText.text = "Correct";
+    }
+    public void WrongAnswer(Button pressedButton, TextMeshProUGUI btnPressedText)
+    {
+        clickBlockPanel.gameObject.SetActive(true);
+        ChangeButtonColor(pressedButton, wrongAnswerColor);
+        btnPressedText.color = Color.white;
+        ChangeButtonColor(answerButtons[3], correctAnswerColor);
+        answerTexts[3].color = Color.white;
+        ChangeButtonColor(nextQuestionButton, wrongAnswerColor);
+        nextQuestionButtonText.color = Color.white;
+        nextQuestionButtonText.text = "Wrong";
+    }
+    public void NextQuestionCountdownStart()
+    {
+        ChangeButtonColor(nextQuestionButton, buttonStartColor);
+        nextQuestionButtonText.color = Color.gray;
+        nextQuestionButtonText.text = "Next question After:" + 3 + " Seconds";
+    }
+    public void NextQuestionCountdownEnd()
+    {
+        clickBlockPanel.SetActive(false);
+        foreach (var button in answerButtons)//reset colors before load new question
+        {
+            ChangeButtonColor(button, buttonStartColor);
+            button.interactable = true;
+        }
+        foreach (var answerText in answerTexts)
+        {
+            answerText.color = answerTextStartColor;
+        }
+    }
+    private void PrintCategory(int questionIndex)
+    {
+        categoryText.text = questionsData.results[questionIndex].category;
+    }
+    private void PrintQuestionNumber(int questionIndex)
+    {
+        questionNumberText.text = (questionIndex + 1) + "/10";
+    }
+    private void PrintQuestion(int questionIndex)
+    {
+        questionText.text = HttpUtility.HtmlDecode(questionsData.results[questionIndex].question);//using striphtml method for remove html entity codes
+    }
+    private void PrintAnswerTexts(int questionIndex)
+    {
+        answerTexts[0].text = HttpUtility.HtmlDecode(questionsData.results[questionIndex].incorrect_answers[0]);
+        answerTexts[1].text = HttpUtility.HtmlDecode(questionsData.results[questionIndex].incorrect_answers[1]);
+        answerTexts[2].text = HttpUtility.HtmlDecode(questionsData.results[questionIndex].incorrect_answers[2]);
+        answerTexts[3].text = HttpUtility.HtmlDecode(questionsData.results[questionIndex].correct_answer);
+    }
+    private void ChangeButtonColor(Button btn, Color color)
+    {
+        ColorBlock colorVar = btn.colors;
+        colorVar.disabledColor = color;
+        btn.colors = colorVar;
     }
 }
